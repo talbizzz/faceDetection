1d1996534c4dee280abe0a8b2e2d17ef65d43fa5
Aziz Talbi <a.talbi890@gmail.com>
2020-09-24 15:43:38 +0200

    face detection box added

--- src/App.js
+++ src/App.js
@@ -31,8 +31,22 @@ class App extends React.Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
   }
+
+  calculateFaceLocation= (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width), 
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  } 
 
   onInputChange= (event) => {
     this.setState({input: event.target.value})
@@ -44,14 +58,13 @@ class App extends React.Component {
       Clarifai.FACE_DETECT_MODEL,
       this.state.input
       )
-      .then(
-        function(response) {
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);        
-        },
-        function(err) {
-        /* there was an error*/
-        }
-      );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))        
+      .catch(err => console.log(err));
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box:box});
   }
 
   render(){
@@ -67,7 +80,7 @@ class App extends React.Component {
           onInputChange={this.onInputChange} 
           onSubmit={this.onSubmit}
         />
-        <FaceRecognition imageUrl= {this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl= {this.state.imageUrl}/>
       </div>
     );
   }
